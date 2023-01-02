573d33f3bc1e8b61a14bb54c476b5bb25cac6c17
mitu-moto <sharmaprathameshm1@gmail.com>
2023-01-02 15:52:19 +0530

    media query writen for mobile menu

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded",function(){
     function showMenu(){
         const menu = document.querySelector("section.menu");
         menu.style.height = "200px";
+        
     }
     
     function closeMenu(){
@@ -14,7 +15,11 @@ document.addEventListener("DOMContentLoaded",function(){
         for(let menuItem of menuItems){
             menuItem.addEventListener("click", closeMenu);
         }}
+
+    
+
     document.getElementById("menu-button").addEventListener("click", showMenu);
     document.getElementById("close-menu").addEventListener("click", closeMenu);
+    
     addClickToMenuItems(); 
 })
